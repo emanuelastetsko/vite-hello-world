6f5b20bc4e9ffc8d9e1f6e3adfe3196708cdc4ec
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-02-02 10:31:22 +0100

    Inizializzazione repo + risoluzione dell'esercizio

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+
+// Importo il componente
+import AppTitle from './components/HelloWorld.vue';
+
+export default {
+  name: 'Hello World',
+  data(){
+
+    return {
+      msg: 'Ciao Vite!',
+      count: 0
+    };
+  },
+
+  // Registro il componente 
+  components: {
+    AppTitle,
+  },
+
+};
+
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+
+  <!--Utilizzo il componente come tag HTML-->
+  <AppTitle />
+</template>
+
+<style>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+  name: 'AppTitle',
+  data(){
+
+    return {
+      msg: 'La mia prima app con Vite!',
+    };
+
+  },
+};
+
+</script>
+
+<template>
+
+  <h1>{{ msg }}</h1>
+
+</template>
+
+<style scoped>
+h1{
+  font-size: 3em;
+}
+</style>
